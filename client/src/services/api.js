eeb2d07bb0edd76723021577ded947b98d4272c9
Bawiloussim <bawiloussimngbabou1@gmail.com>
2025-08-22 00:46:47 +0000

    api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({
-    baseURL: 'http://localhost:3000/api',
+const API = axios.create({ 
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use(cfg => {
